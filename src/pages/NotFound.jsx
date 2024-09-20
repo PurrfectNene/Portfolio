9a672b5129bae6265084a62d91bda4360435bc0e
Irene <amoreperdesi@gmail.com>
2024-09-20 19:27:26 +0200

    notfound style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NotFound() {
   return (
     <div>
-    <img src='../../public/Images/NoFound.png' alt='Not found'/>
+    <img src='../../public/Images/NotFound.png' alt='Not found'/>
     </div>
   )
 }
